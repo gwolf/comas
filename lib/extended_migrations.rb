56e6c7473c2ab00db4568f0a6c5831b38eb56849
Gunnar Wolf <gwolf@gwolf.org>
2008-01-17 16:20:15 +0000

    Documentation extended

--- lib/extended_migrations.rb
+++ lib/extended_migrations.rb
@@ -1,8 +1,8 @@
 #:title: Extended migrations
 #
-# This library extends the base Rails ActiveRecord::Migration with some 
-# commonly used functionality in, easily grouped, aiming at keeping your 
-# migrations short and easy to understand.
+# This library extends the base <tt>Rails ActiveRecord::Migration</tt>
+# with some commonly used functionality in, easily grouped, aiming at keeping
+# your  migrations short and easy to understand.
 #
 # = Catalogs
 # 
@@ -25,20 +25,73 @@
 #
 # So, if you want to create a relation field, properly acting as a foreign
 # key and refering to another table, instead of doing it via the 
-# ActiveRecord::ConnectionAdapters::TableDefinition#column (or even 
-# ActiveRecord::ConnectionAdapters::TableDefinition#references) methods,
-# declare them this way:
+# <tt>ActiveRecord::ConnectionAdapters::TableDefinition#column</tt> (or even 
+# <tt>ActiveRecord::ConnectionAdapters::TableDefinition#references</tt>) 
+# methods, declare them with +add_refrence+:
 #
 #   def self.up
+#     create_catalogs :prop_types
 #     create_table :proposals do |t|
 #       (...)
 #     end
+#     add_reference(:proposals, :prop_types)
+#     add_reference(:proposals, :prop_statuses, :null => false, :default => 1)
 #   end
-#   add_reference(:proposals, :prop_types)
-#   add_reference(:proposals, :prop_statuses, :null => false, :default => 1)
 #
 # The corresponding fields (+prop_type_id+ and +prop_status_id+, respectively) 
 # will be created, and the foreign key will be set.
+# 
+# The references can be removed via +remove_reference+, i.e., for inclusion in 
+# +down+ migrations:
+# 
+#   def self.down
+#     remove_reference(:proposals, :prop_types)
+#     drop_table :proposals
+#     drop_catalogs :prop_types
+#   end
+# 
+# Of course, in this case the +remove_reference+ call is not really needed - But
+# if you are dropping a table that is referred to from a table that will 
+# remain existing, you will have to remove the foreign key constraints (that 
+# is, the referring columns).
+#
+# = Join (HABTM) tables
+#
+# When you define a simple +has_and_belongs_to_many+ (HABTM) relation in your 
+# model, you are expected to create an extra table representing this relation.
+# This is a very simple table (i.e. it has only a row for each of the related
+# table's IDs), and it carries foreign key constraints (see the +References+ 
+# section). 
+#
+# Please note that join tables <i>are not supposed to carry any extra 
+# columns</i> - If you need to add information to join tables, think twice and
+# better use a <tt>has_many :elements, :through => :othermodel</tt> 
+# declaration, and create the +othermodel+ table manually.
+# 
+# To create a HABTM table representing that each person can attend many 
+# conferences and each conference can be attended by many people:
+#
+#   def self.up
+#     create_table :people do |t|
+#       (...)
+#     end
+#     create_table :conferences do |t|
+#       (...)
+#     end
+#     create_habtm :people, :conferences
+#   end
+#   def self.down
+#     drop_habtm :people, :conferences
+#     drop_table :people
+#     drop_table :conferences
+#   end
+#
+# The last call will create a +conferences_people+ table (according to Rails'
+# conventions, table names are sorted alphabetically to get the join table's
+# name).
+# 
+# Note that in the +down+ migration, the +drop_habtm+ call <i>must appear 
+# before</i> the +drop_table+ calls, as it carries foreign key constraints.
 module ActiveRecord
   module ConnectionAdapters # :nodoc:
     module SchemaStatements
@@ -103,7 +156,7 @@ module ActiveRecord
       # only one relation is created between any two given records, and two
       # regular indexes, to ensure speedy lookups.
       def create_habtm(first, second)
-        first, second = second, first if first.to_s > second.to_s 
+        first, second = sort_tables(first, second)
         first_fld = fldname(first)
         second_fld = fldname(second)
         join_tbl = "#{first}_#{second}"
@@ -120,7 +173,7 @@ module ActiveRecord
 
       # Drops a HABTM join-table between the two given tables.
       def drop_habtm(first, second)
-        first, second = second, first if first.to_s > second.to_s 
+        first, second = sort_tables(first, second)
         drop_table "#{first}_#{second}"
       end
 
@@ -128,6 +181,12 @@ module ActiveRecord
       #:nodoc:
       def fldname(tbl)
         "#{tbl.to_s.singularize}_id"
+      end
+
+      #:nodoc:
+      def sort_tables(first, second)
+        first, second = second, first if first.to_s > second.to_s 
+        return first, second
       end
     end
   end
