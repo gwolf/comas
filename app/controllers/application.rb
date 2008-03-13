d5a12289f8dda4fac3fe37392676039f0f4abbae
Gunnar Wolf <gwolf@gwolf.org>
2008-03-13 21:29:22 +0000

    Removed the 'globalize' plugin, as it proves too intrusive. Maybe later it will make a comeback..?

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_comas_session_id'
 
-  before_filter :set_locale
   before_filter :get_user
   before_filter :generate_menu
 
@@ -21,19 +20,5 @@ class ApplicationController < ActionController::Base
              {:label => 'Log in',
                :link => {:controller => 'people', :action => 'login'}},
             ]
-  end
-
-  def set_locale
-    # Set to the locale to whatever is specified in the :lang parameter. Not 
-    # set? Ok, then go to the last locale set in the session. Not set? Bah, 
-    # default to English.
-    default = 'en-US'
-
-    locale = params[:lang] || session[:lang] || default
-    locale = default unless RFC_3066.valid?(locale)
-
-    Locale.set(locale)
-    session[:lang] = locale
-    true
   end
 end
