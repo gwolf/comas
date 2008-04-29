1b86960093c879d604738e1c53c590e2eafb48e6
Gunnar Wolf <gwolf@gwolf.org>
2008-04-29 17:01:59 +0000

    Creating core administrative tasks from within its migration

--- db/migrate/005_admin_tasks.rb
+++ db/migrate/005_admin_tasks.rb
@@ -2,6 +2,9 @@ class AdminTasks < ActiveRecord::Migration
   def self.up
     create_catalogs :admin_tasks
     create_habtm :admin_tasks, :people
+
+    ['people_adm', 'conferences_adm', 'academic', 'attendance', 
+     'sys_conf'].each {|at| AdminTask.new(:name => at).save!}
   end
 
   def self.down
