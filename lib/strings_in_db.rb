faa181164d56e0a1f13bde04b75359e6e0070350
Gunnar Wolf <gwolf@gwolf.org>
2008-05-06 18:44:53 +0000

    (incomplete) strings of default DB-based fields, only used to generate the gettext templates

---
+++ lib/strings_in_db.rb
@@ -0,0 +1,26 @@
+# Just list here the strings that will appear in the database in a
+# standard Comas installation. Of course, we could just manually add
+# them to the .po - but it's easier to have them all refered from
+# here.
+
+# Person types
+_('PersonType|Attendee')
+_('PersonType|Speaker')
+_('PersonType|Organizer')
+_('PersonType|Moderator')
+_('PersonType|Guest')
+_('PersonType|Press')
+
+# Proposal types
+_('PropType|Round table')
+_('PropType|Tutorial')
+_('PropType|Talk')
+_('PropType|Workshop')
+
+# Proposal statuses
+_('PropStatus|Accepted')
+_('PropStatus|Cancelled')
+_('PropStatus|Further details required')
+_('PropStatus|New')
+_('PropStatus|Rejected')
+_('PropStatus|Withdrawn')
