4373f4ef9fe38a49e8222e74ea789686b2f85339
Gunnar Wolf <gwolf@gwolf.org>
2009-02-24 13:09:59 -0600

    Added indexing to the email fields in the DB

--- db/migrate/011_email_opt_in_fields.rb
+++ db/migrate/011_email_opt_in_fields.rb
@@ -2,6 +2,8 @@ class EmailOptInFields < ActiveRecord::Migration
   def self.up
     add_column :people, :ok_conf_mails, :boolean, :default => false
     add_column :people, :ok_general_mails, :boolean, :default => false
+    add_index :people, :ok_conf_mails
+    add_index :people, :ok_general_mails
   end
 
   def self.down
