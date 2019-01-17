2b3d4e0f412dcb21d1ab7cbe0ea5645cef438c1a
Gunnar Wolf <gwolf@gwolf.org>
2019-01-17 14:25:02 -0600

    Typo

--- db/migrate/025_min_attendances_for_certif.rb
+++ db/migrate/025_min_attendances_for_certif.rb
@@ -1,6 +1,6 @@
 class MinAttendancesForCertif < ActiveRecord::Migration
   def self.up
-    add_column :conference, :min_attendances, :integer, :default => 0, :null => false
+    add_column :conferences, :min_attendances, :integer, :default => 0, :null => false
   end
   def self.down
     remove_column :conferences, :min_attendances
